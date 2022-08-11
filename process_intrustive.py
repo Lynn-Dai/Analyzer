33eb78759bea6f28d0fc09ae354ea2993f9d7b33
Lynn Dai <daiyitong0808@stu.xjtu.edu.cn>
2022-08-11 18:28:40 +0800

    Update the process of coupling pattern...

--- process_intrustive.py
+++ process_intrustive.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 path = 'E:/ASE/实验数据/3-侵入式修改'
 Intrusive_files = 'ownership_file_count'
+coupling_files = 'file-pattern'
 conflicts_path = 'E:/PycharmProjects/Analyzer/conflicts'
 intrusives = defaultdict(list)
 conffiles = defaultdict(list)
@@ -29,7 +30,7 @@ def get_intrusive_files(ownership_file: str):
                 intrusive.append(row['file'])
     return intrusive
 
-def get_merge_files():
+def get_conf_csvs():
     for root, lists, files in os.walk(conflicts_path):
         for file in files:
             version = file.split('-merge')[0]
@@ -43,11 +44,71 @@ def get_conf_files(merge_file: str):
     data = pd.read_csv(merge_file, usecols=[4])
     for index, row in data.iterrows():
         # print(row['Conf_details'])
-        for item in row['Conf_details'][1:-1].split(','):
+        for item in row['Conf_details'][1:-1].split(', '):
             if item != '':
+                # print(item)
                 confs.append(item[1:-1])
     return confs
 
+
+def get_conf_csv(version_couple: str):
+    for root, lists, files in os.walk(conflicts_path):
+        for file in files:
+            version = file.split('-merge')[0]
+            write = os.path.join(root, file)
+            if version_couple in version:
+                return write
+
+def process_file_conf_times(data: pd.DataFrame, file_path: str):
+    times = 0
+    for index, row in data.iterrows():
+        for item in row['Conf_details'][1:-1].split(', '):
+            if file_path in item:
+                times = times + 1
+    return times
+
+
+def process_coupling_pattern():
+    for root, lists, files in os.walk(path):
+        for file in files:
+            if coupling_files in file:
+                conf_times = []
+                version = root.split('\\')[-1]
+                write = os.path.join(root, file)
+                print('%s %s' % (version, write))
+                coupling_data = pd.read_csv(write)
+                conf_data = pd.read_csv(get_conf_csv(version))
+                # Go through the whole coupling files in current version
+                for coupling_file in coupling_data['filename']:
+                    conf_times.append(process_file_conf_times(conf_data, coupling_file))
+                coupling_data['Conflict_times'] = conf_times
+                coupling_data.to_csv(os.path.join('./coupling/', version+'.csv', ))
+
+
+def conf_vs_intrusive():
+    result = []
+    for conf_version,conf_files in conffiles.items():
+        for intru_version,intru_files in intrusives.items():
+            if intru_version in conf_version:
+                version = [conf_version]
+                conf_intrusive = []
+                conf_not_intrusive = []
+                for conf_file in conf_files:
+                    if conf_file in intru_files:
+                        conf_intrusive.append(conf_file)
+                    else:
+                        conf_not_intrusive.append(conf_file)
+                version.append(len(conf_intrusive))
+                version.append(len(conf_not_intrusive))
+                version.append(str(conf_intrusive))
+                version.append(str(conf_not_intrusive))
+                result.append(version)
+    return result
+
+def output_intrusive(data):
+    name_attribute = ['Project', 'Conf_intrusive', 'Conf_not_intrusive', 'Conf_intrusive_detail', 'Conf_not_intrusive_details']
+    writerCSV = pd.DataFrame(columns=name_attribute, data=data)
+    writerCSV.to_csv('./conf_vs_intrusive.csv', encoding='utf-8')
 
 def list_to_csv(csv_path, data):
     with open(csv_path, 'w') as f:
@@ -55,7 +116,9 @@ def list_to_csv(csv_path, data):
 
 
 if __name__ == '__main__':
-    # list_to_csv('./Intrusive_files.csv', get_final_ownership())
-    # list_to_csv('./conf_files.csv', get_merge_files())
-    get_final_ownership()
-    get_merge_files()
+    list_to_csv('./Intrusive_files.csv', get_final_ownership())
+    list_to_csv('./conf_files.csv', get_conf_csvs())
+    # get_final_ownership()
+    # get_merge_files()
+    output_intrusive(conf_vs_intrusive())
+    process_coupling_pattern()
