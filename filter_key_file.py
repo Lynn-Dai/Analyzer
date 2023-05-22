8694d8f6cb9530bb72a0eb2835b20a953a66a458
Lynn Dai <lynnd0808@163.com>
2023-05-22 22:45:26 +0800

    Update scripts.

--- filter_key_file.py
+++ filter_key_file.py
@@ -20,13 +20,19 @@ def filter_json(path: str):
             entities[entity['id']] = entity
             if entity['File'] in key_files:
                 key_entities.append(entity['id'])
-                # signature = entity['qualifiedName']
-                # if entity['category'] == "Method":
-                #     signature += "(" + entity['parameter']['types'] + ")"
-                # key_entities_qualifiedName.append(signature)
         for relation in data['cells']:
             if relation['src'] in key_entities:
                 src_entity = entities[relation['src']]
+                src_signature = src_entity['qualifiedName']
+                if src_entity['category'] == "Method":
+                    src_signature += "(" + src_entity['parameter']['types'] + ")"
+                # key_entities_qualifiedName.append(signature)
+                dest_entity = entities[relation['dest']]
+                dest_signature = dest_entity['qualifiedName']
+                if dest_entity['category'] == "Method":
+                    dest_signature += "(" + dest_entity['parameter']['types'] + ")"
+                value = relation['values'].keys().remove('loc')
+
 
 
 
